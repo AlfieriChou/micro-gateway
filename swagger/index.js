bc1f3c95b92e4fad704e4c8290e21475a7fe10df
AlfieriChou <alfierichou@gmail.com>
2018-11-18 20:36:35 +0800

    rewrite generate swagger.json

--- swagger/index.js
+++ swagger/index.js
@@ -4,6 +4,75 @@ const _ = require('lodash')
 const appRoot = require('app-root-path')
 const dir = require('dir_filenames')
 
+const generateComponents = (model, modelName) => {
+  const modelSchema = convert(model)
+  let schema = {}
+  schema[modelName] = {
+    'type': 'object',
+    'properties': modelSchema.properties
+  }
+  return schema
+}
+
+const generateInterface = (model) => {
+  const content = {
+    tags: model.tags,
+    summary: model.summary,
+    description: model.description
+  }
+
+  if (model.query) {
+    content.parameters = []
+    let params = convert(Joi.object(model.query))
+    for (let prop in params.properties) {
+      let field = {}
+      field.name = prop
+      field.in = 'query'
+      field.description = params.properties[prop].description
+      field.schema = {
+        'type': params.properties[prop].type
+      }
+      field.required = false
+      content.parameters.push(field)
+    }
+  }
+
+  if (model.params) {
+    content.parameters = []
+    let params = convert(Joi.object(model.params))
+    for (let prop in params.properties) {
+      let field = {}
+      field.name = prop
+      field.in = 'path'
+      field.description = params.properties[prop].description
+      field.schema = {
+        'type': params.properties[prop].type
+      }
+      field.required = true
+      content.parameters.push(field)
+    }
+  }
+
+  if (model.requestBody) {
+    let params = convert(Joi.object(model.requestBody.body))
+    let request = {}
+    request.requestBody = {}
+    let bodySchema = request.requestBody
+    bodySchema.required = true
+    bodySchema.content = {
+      'application/json': {
+        'schema': {
+          'type': params.type,
+          'properties': params.properties,
+          'required': model.requestBody.required
+        }
+      }
+    }
+    content.requestBody = request.requestBody
+  }
+  return content
+}
+
 const generateSwagger = (info) => {
   const items = dir(`${appRoot}/swagger/models`)
   _.remove(items, (n) => {
@@ -18,70 +87,10 @@ const generateSwagger = (info) => {
     let schemaName = fileName.slice(0, 1).toUpperCase() + fileName.slice(1)
     for (let index in model) {
       if (index === 'schema') {
-        const modelSchema = convert(model[index])
-        let schema = {}
-        schema[schemaName] = {
-          'type': 'object',
-          'properties': modelSchema.properties
-        }
+        const schema = generateComponents(model[index], schemaName)
         components.schemas = _.merge(components.schemas, schema)
       } else {
-        const content = {
-          tags: model[index].tags,
-          summary: model[index].summary,
-          description: model[index].description
-        }
-
-        if (model[index].query) {
-          content.parameters = []
-          let params = convert(Joi.object(model[index].query))
-          for (let prop in params.properties) {
-            let field = {}
-            field.name = prop
-            field.in = 'query'
-            field.description = params.properties[prop].description
-            field.schema = {
-              'type': params.properties[prop].type
-            }
-            field.required = false
-            content.parameters.push(field)
-          }
-        }
-
-        if (model[index].params) {
-          content.parameters = []
-          let params = convert(Joi.object(model[index].params))
-          for (let prop in params.properties) {
-            let field = {}
-            field.name = prop
-            field.in = 'path'
-            field.description = params.properties[prop].description
-            field.schema = {
-              'type': params.properties[prop].type
-            }
-            field.required = true
-            content.parameters.push(field)
-          }
-        }
-
-        if (model[index].requestBody) {
-          let params = convert(Joi.object(model[index].requestBody.body))
-          let request = {}
-          request.requestBody = {}
-          let bodySchema = request.requestBody
-          bodySchema.required = true
-          bodySchema.content = {
-            'application/json': {
-              'schema': {
-                'type': params.type,
-                'properties': params.properties,
-                'required': model[index].requestBody.required
-              }
-            }
-          }
-          content.requestBody = request.requestBody
-        }
-
+        const content = generateInterface(model[index])
         const schema = convert(model[index].output)
         content.responses = {
           200: {
