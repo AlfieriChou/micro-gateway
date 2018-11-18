e8ad0fe12440e62a89462fdcd9ea0878a024f477
AlfieriChou <alfierichou@gmail.com>
2018-11-18 21:09:52 +0800

    micro dev

---
+++ server.js
@@ -0,0 +1,25 @@
+module.exports = {
+  host: 'localhost',
+  rules: [{
+    slug: 'hello',
+    pathname: '/api/hello',
+    dest: 'hello.api.localhost',
+    method: ['GET'],
+    run: 'cd ./app/hello && micro-dev hello.js -p 3001',
+    debug: true
+  }, {
+    slug: 'captcha',
+    pathname: '/api/captcha',
+    dest: 'captcha.api.localhost',
+    method: ['GET'],
+    run: 'cd ./app/captcha && micro-dev captcha.js -p 3002',
+    debug: true
+  }, {
+    slug: 'swagger',
+    pathname: '/api/swagger.json',
+    dest: 'swagger.api.localhost',
+    method: ['GET'],
+    run: 'cd ./app/swagger && micro-dev swagger.js -p 3003',
+    debug: true
+  }]
+}
